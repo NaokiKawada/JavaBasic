6ed1cf11edc77b1389391830af09ec4a116deacc
naoki-kawada <naoki.net12@gmail.com>
2018-01-18 18:01:23 +0900

    PTra10_02まで完了

--- Practice/src/practice10/PTra10_01.java
+++ Practice/src/practice10/PTra10_01.java
@@ -8,29 +8,26 @@ package practice10;
 
 public class PTra10_01 {
 
-	/*
-	 * ★ 以下の仕様のクラスを作成してください（新しくJavaファイルを作成してください）
-	 *
-	 * クラス名
-	 * 		User
-	 * フィールド
-	 * 		userId		:	int型
-	 * 		userNm		：	String型
-	 * 		mail		：	String型
-	 * 		password	:	String型
-	 * メソッド
-	 * 		なし
-	 */
-
 	public static void main(String[] args) {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
+		User us;
 
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
 
+		us = new User();
+		us.userId = 57143;
+		us.userNm = "locatelli";
+		us.mail = "iiiii@gmail.com";
+		us.password = "tli22";
+
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+        System.out.println(us.userId);
+        System.out.println(us.userNm);
+        System.out.println(us.mail);
+        System.out.println(us.password);
 	}
 }
+
